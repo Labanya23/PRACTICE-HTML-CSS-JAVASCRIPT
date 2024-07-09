7c3ed5db5309cbfa041e5f441be48563e1d0e06f
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2024-07-09 22:11:06 +0600

    Add files via upload

---
+++ Practice1/main.js
@@ -0,0 +1,9 @@
+document.getElementById("doraemon").style.display="none";
+
+function displayImage(){
+    document.getElementById("doraemon").style.display="block";
+    
+}
+
+
+document.getElementById("btnn").onclick=displayImage;
